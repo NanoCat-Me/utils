2059991dbb824b7c957392cf5adc3fb1c90e5396
Virgil Clyne <Virgil@nanocat.me>
2024-09-05 10:51:35 -0300

    URL

--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,5 @@
 import { $platform } from "./platform.mjs";
+import URL from "./URL.mjs";
 import _ from "./Lodash.mjs";
 import Storage from "./Storage.mjs";
 import fetch from "./fetch.mjs";
@@ -9,4 +10,4 @@ export const wait = (delay = 1000) => new Promise(resolve => setTimeout(resolve,
 import done from "./done.mjs";
 export const getScript = async (url) => await fetch(url).then(response => response.body);
 import runScript from "./runScript.mjs";
-export { $platform, _, Storage, fetch, notification, logError, done, runScript };
+export { $platform, URL, _, Storage, fetch, notification, logError, done, runScript };
