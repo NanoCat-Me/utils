1f5a6780a18c0063eb967af6fdcfb5143ac23559
Virgil Clyne <Virgil@nanocat.me>
2024-09-19 12:38:23 +0800

    done
    Update done.mjs

--- done.mjs
+++ done.mjs
@@ -1,25 +1,29 @@
 import { $platform, _, log } from "./utils.mjs";
 
 export default function done(object = {}) {
-    log("", `🚩 执行结束!`, "");
     switch ($platform) {
         case "Surge":
             if (object.policy) _.set(object, "headers.X-Surge-Policy", object.policy);
+            log("", `🚩 执行结束! 🕛 ${(new Date().getTime() / 1000 - $script.startTime)} 秒`, "");
             $done(object);
             break;
         case "Loon":
             if (object.policy) object.node = object.policy;
+            log("", `🚩 执行结束! 🕛 ${(new Date() - $script.startTime) / 1000} 秒`, "");
             $done(object);
             break;
         case "Stash":
             if (object.policy) _.set(object, "headers.X-Stash-Selected-Proxy", encodeURI(object.policy));
+            log("", `🚩 执行结束! 🕛 ${(new Date() - $script.startTime) / 1000} 秒`, "");
             $done(object);
             break;
         case "Egern":
+            log("", `🚩 执行结束!`, "");
             $done(object);
             break;
         case "Shadowrocket":
         default:
+            log("", `🚩 执行结束!`, "");
             $done(object);
             break;
         case "Quantumult X":
@@ -47,9 +51,11 @@ export default function done(object = {}) {
                 object.bodyBytes = object.body.buffer.slice(object.body.byteOffset, object.body.byteLength + object.body.byteOffset);
                 delete object.body;
             } else if (object.body) delete object.bodyBytes;
+            log("", `🚩 执行结束!`, "");
             $done(object);
             break;
         case "Node.js":
+            log("", `🚩 执行结束!`, "");
             process.exit(1);
             break;
     }
